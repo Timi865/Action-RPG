133909bb1728b6617775041573a0a1bf514efc96
Timi865 <timiboris@outlook.com>
2026-02-08 17:59:09 +0000

    Added a basic state machine setup to the player script
    Added a basic State machine, if/Elif setup to the player script. currently AttackState is not doing anything in the code

--- player/player.gd
+++ player/player.gd
@@ -5,17 +5,21 @@ const SPEED = 100.0
 var input_vector: = Vector2.ZERO#by putting this variable at the top this means that i can use it in all functions
 
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
 
 func _physics_process(_delta: float) -> void:
-	
-	input_vector = Input.get_vector("move_left","move_right", "move_up", "move_down")#WHOA This is legit movement 
-	
-	if input_vector != Vector2.ZERO:
-		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-		update_blend_positions(direction_vector)
-	
-	velocity = input_vector * SPEED
-	move_and_slide()
+	var state = playback.get_current_node()
+	if state == "MoveState":
+		input_vector = Input.get_vector("move_left","move_right", "move_up", "move_down")#WHOA This is legit movement 
+		
+		if input_vector != Vector2.ZERO:
+			var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+			update_blend_positions(direction_vector)
+		
+		velocity = input_vector * SPEED
+		move_and_slide()
+	elif state == "AttackState":
+		pass
 	
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_vector)
