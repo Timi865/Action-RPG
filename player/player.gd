2cd8695808069beaa5fc51b6a940227b6df9d649
Timi865 <timiboris@outlook.com>
2026-02-08 18:16:19 +0000

    Used Match instead of if/elif
    Used Match instead of if/elif in state machine for player code

--- player/player.gd
+++ player/player.gd
@@ -9,20 +9,21 @@ var input_vector: = Vector2.ZERO#by putting this variable at the top this means 
 
 func _physics_process(_delta: float) -> void:
 	var state = playback.get_current_node()
-	if state == "MoveState":
-		input_vector = Input.get_vector("move_left","move_right", "move_up", "move_down")#WHOA This is legit movement 
-		
-		if input_vector != Vector2.ZERO:
-			var direction_vector: = Vector2(input_vector.x, -input_vector.y)
-			update_blend_positions(direction_vector)
+	match state:
+		"MoveState":
+			input_vector = Input.get_vector("move_left","move_right", "move_up", "move_down")#WHOA This is legit movement 
 			
-		if Input.is_action_just_pressed("attack"):
-			playback.travel("AttackState")
-		
-		velocity = input_vector * SPEED
-		move_and_slide()
-	elif state == "AttackState":
-		pass
+			if input_vector != Vector2.ZERO:
+				var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+				update_blend_positions(direction_vector)
+				
+			if Input.is_action_just_pressed("attack"):
+				playback.travel("AttackState")
+			
+			velocity = input_vector * SPEED
+			move_and_slide()
+		"AttackState":
+			pass
 	
 func update_blend_positions(direction_vector: Vector2) -> void:
 	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_vector)
