32cf5b950a3700567ac7557489f8cf8212bcb1f2
Timi865 <timiboris@outlook.com>
2026-02-08 17:20:55 +0000

    Initial commit

---
+++ player/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+var input_vector: = Vector2.ZERO#by putting this variable at the top this means that i can use it in all functions
+
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+func _physics_process(_delta: float) -> void:
+	
+	input_vector = Input.get_vector("move_left","move_right", "move_up", "move_down")#WHOA This is legit movement 
+	
+	if input_vector != Vector2.ZERO:
+		var direction_vector: = Vector2(input_vector.x, -input_vector.y)
+		update_blend_positions(direction_vector)
+	
+	velocity = input_vector * SPEED
+	move_and_slide()
+	
+func update_blend_positions(direction_vector: Vector2) -> void:
+	animation_tree.set("parameters/StateMachine/MoveState/RunState/blend_position", input_vector)
+	animation_tree.set("parameters/StateMachine/MoveState/StandState/blend_position", input_vector)
