ab167f517797c59d526326f87e7b905e633651e6
Timi865 <timiboris@outlook.com>
2026-03-14 17:53:33 +0000

    connected previous signal through code and not through the editor

--- grass.gd
+++ grass.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
+@onready var area_2d: Area2D = $Area2D#this accesses the area 2d so i can use it in the script
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	area_2d.area_entered.connect(_on_area_2d_area_entered)#this connects the signal in brackets to the code WITH code instead of from the editor which is more reliable:>
 
-
-func _on_area_2d_area_entered(area: Area2D) -> void:
+func _on_area_2d_area_entered(other_area_2d: Area2D) -> void:
 	queue_free()
 	print("grass area entered")
-	
