de2582898e0cf501e30d4ee2b9d67591f0401f2d
Timi865 <timiboris@outlook.com>
2026-03-30 10:36:25 +0100

    added a class name to player
    added a class name to the player and used it to reference the player in my bat script with an export var

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
+@export var player: Player
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
