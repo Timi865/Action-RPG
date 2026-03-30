e192cb8707114b83032e8197422c25184389768f
Timi865 <timiboris@outlook.com>
2026-03-30 10:49:20 +0100

    accessed the player in a different way
    accessed the player in a different way that is more useful. i put the player in its own global group

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -3,10 +3,13 @@ extends CharacterBody2D
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
-@export var player: Player
+
 
 func _physics_process(delta: float) -> void:
 	var state = playback.get_current_node()
 	match state:
 		"Idle" : pass
 		"Chase": pass
+
+func get_player() -> Player:
+	return get_tree().get_first_node_in_group("player")
