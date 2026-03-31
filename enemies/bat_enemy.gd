a0eb80e0e915ae70cd4381a49babe1e2b8082c8b
Timi865 <timiboris@outlook.com>
2026-03-31 15:52:27 +0100

    Update bat_enemy.gd
    wrote a function that checks if the player is in range of the bat

--- enemies/bat_enemy.gd
+++ enemies/bat_enemy.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var aggrorange: = 128
+
 @onready var sprite_2d: Sprite2D = $Sprite2D
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/StateMachine/playback") as AnimationNodeStateMachinePlayback
@@ -13,3 +15,12 @@ func _physics_process(delta: float) -> void:
 
 func get_player() -> Player:
 	return get_tree().get_first_node_in_group("player")
+
+func is_player_in_range() -> bool:
+	var result = false
+	var player: = get_player()
+	if player is Player:#this checks if the player is a player and if exists
+		var distance_to_player = global_position.distance_to(player.global_position)
+		if distance_to_player < aggrorange:
+			result = true
+	return result
